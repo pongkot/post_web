0e15d015307602c84198a4076d881d01b161bd83
Pongkot Sae-li <pongkot.saeli@outlook.com>
2023-11-12 13:07:28 +0700

    feat: add author, date and more

--- post/models.py
+++ post/models.py
@@ -8,6 +8,20 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     body = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    comment_text = models.CharField(max_length=100)
+    user_rating = models.IntegerField()
+    commented_post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="post_comment"
+    )
+    is_approved = models.BooleanField()
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment_text
--- post/views.py
+++ post/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import ListView
+
 from .models import Post
 
 
