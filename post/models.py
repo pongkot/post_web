8cb0fcdd64328786d7daa9af762e04124d61f177
Pongkot Sae-li <pongkot.saeli@outlook.com>
2023-11-13 09:02:43 +0700

    feat: add comment

--- post/models.py
+++ post/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -12,6 +13,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_update", args=[str(self.id)])
 
 
 class Comment(models.Model):
